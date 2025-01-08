eab30acc43c7b8dbaa19c3fe43bb009322361fed
Totto16 <tobiausgais@gmail.com>
2025-01-08 22:32:50 +0100

    fix: remove eslint config errors and use ts preset

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
-import babelParser from 'babel-eslint'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -13,12 +14,11 @@ const compat = new FlatCompat({
 })
 
 export default [
-    ...compat.extends('airbnb-base'),
+    ...tseslint.config(
+        eslint.configs.recommended,
+        tseslint.configs.recommended
+    ),
     {
-        languageOptions: {
-            parser: babelParser,
-        },
-
         rules: {
             'no-plusplus': [
                 'error',
